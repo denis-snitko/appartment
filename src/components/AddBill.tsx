198474ada5baad161c94607accbe5667897f18a5
denis-snitko <denissnitko@gmail.com>
2023-01-28 16:43:58 +0300

    feat: default paid value

--- src/components/AddBill.tsx
+++ src/components/AddBill.tsx
@@ -1,176 +1,181 @@
-import { ChangeEvent, FC, useRef, useState } from "react";
+import { ChangeEvent, FC, useEffect, useRef, useState } from 'react';
 import {
-	Button,
-	FormControl,
-	FormLabel,
-	Input,
-	Textarea,
-	Modal,
-	ModalBody,
-	ModalCloseButton,
-	ModalContent,
-	ModalFooter,
-	ModalHeader,
-	ModalOverlay,
-	useDisclosure,
-} from "@chakra-ui/react";
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from '@chakra-ui/react';
 
-import {
-	getDownloadURL,
-	ref as refStorage,
-	uploadBytes,
-} from "firebase/storage";
-import { push, ref } from "firebase/database";
-import { database, storage } from "../firebase";
-import FileUpload from "./FileUpload";
+import { getDownloadURL, ref as refStorage, uploadBytes } from 'firebase/storage';
+import { push, ref } from 'firebase/database';
+import { database, storage } from '../firebase';
+import FileUpload from './FileUpload';
 import { useData } from '../store/store';
 
 type AddBillModalProps = {
-	pathname: string;
+  pathname: string;
 };
 
 const AddBillModal: FC<AddBillModalProps> = ({ pathname }) => {
-	const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const entities = useData((state: any) => state?.entities);
+  const lastPaid = entities?.[entities.length - 1]?.paid;
 
-	const [data, setData] = useState({
-		date: "",
-		billUrl: "",
-		comment: "",
-		paid: 0,
-	});
-	const [isLoading, setIsLoading] = useState(false);
-	
-	const fetchData = useData((state: any) => state.fetchData);
-	
-	const initialRef = useRef(null);
+  const [data, setData] = useState({
+    date: '',
+    billUrl: '',
+    comment: '',
+    paid: 0,
+  });
 
-	const changeHandler = (
-		event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>
-	) => {
-		const { name, value } = event.target;
-		if (name === "paid") {
-			setData((prev) => ({ ...prev, [name]: Number(value) }));
-		} else {
-			setData((prev) => ({ ...prev, [name]: value }));
-		}
-	};
+  const [isLoading, setIsLoading] = useState(false);
 
-	const uploadHandler = (event: any) => {
-		setIsLoading(true);
-		const { name } = event.target;
-		const file = event.target.files[0];
-		const storageRef = refStorage(storage, `bills/${file.name}`);
+  const fetchData = useData((state: any) => state.fetchData);
 
-		uploadBytes(storageRef, file).then((snapshot) => {
-			getDownloadURL(refStorage(storage, `${snapshot.metadata.fullPath}`))
-				.then((result) => {
-					setData((prev) => ({ ...prev, [name]: result }));
-				})
-				.then(() => setIsLoading(false));
-		});
-	};
+  const initialRef = useRef(null);
 
-	const submitHandler = (pathname: string) => {
-		const { date, paid } = data;
+  const changeHandler = (event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => {
+    const { name, value } = event.target;
+    if (name === 'paid') {
+      setData((prev) => ({ ...prev, [name]: Number(value) }));
+    } else {
+      setData((prev) => ({ ...prev, [name]: value }));
+    }
+  };
 
-		if (!date || !paid) {
-			return;
-		}
+  const uploadHandler = (event: any) => {
+    setIsLoading(true);
+    const { name } = event.target;
+    const file = event.target.files[0];
+    const storageRef = refStorage(storage, `bills/${file.name}`);
 
-		push(ref(database, "apartment" + pathname), data)
-			.then(() => {
-				setData({ date: "", billUrl: "", comment: "", paid: 0 });
-				onClose();
-			})
-			.then(() => {
-				fetchData(pathname).then(() => {});
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-	};
+    uploadBytes(storageRef, file).then((snapshot) => {
+      getDownloadURL(refStorage(storage, `${snapshot.metadata.fullPath}`))
+        .then((result) => {
+          setData((prev) => ({ ...prev, [name]: result }));
+        })
+        .then(() => setIsLoading(false));
+    });
+  };
 
-	return (
-		<>
-			<Button variant="solid" colorScheme="teal" size="sm" onClick={onOpen}>
-				Добавить
-			</Button>
+  const submitHandler = (pathname: string) => {
+    const { date, paid } = data;
 
-			<Modal initialFocusRef={initialRef} isOpen={isOpen} onClose={onClose}>
-				<ModalOverlay />
-				<ModalContent>
-					<ModalHeader>Добавить счёт</ModalHeader>
-					<ModalCloseButton />
-					<ModalBody pb={6}>
-						<FormControl>
-							<FormLabel>Дата</FormLabel>
-							<Input
-								type="date"
-								ref={initialRef}
-								placeholder="Дата"
-								name="date"
-								onChange={(event) => {
-									changeHandler(event);
-								}}
-							/>
-						</FormControl>
+    console.log(data);
+    if (!date || !paid) {
+      return;
+    }
 
-						<FormControl mt={4}>
-							<FormLabel>Загрузить чек</FormLabel>
-							{/* <Input
+    push(ref(database, 'apartment' + pathname), data)
+      .then(() => {
+        setData({ date: '', billUrl: '', comment: '', paid: 0 });
+        onClose();
+      })
+      .then(() => {
+        fetchData(pathname).then(() => {});
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    if (pathname === '/moneybox') {
+      setData((prev) => ({ ...prev, paid: lastPaid + 0.01 }));
+    } else {
+      setData((prev) => ({ ...prev, paid: lastPaid }));
+    }
+  }, [lastPaid, pathname]);
+
+  return (
+    <>
+      <Button variant="solid" colorScheme="teal" size="sm" onClick={onOpen}>
+        Добавить
+      </Button>
+
+      <Modal initialFocusRef={initialRef} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Добавить счёт</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <FormLabel>Дата</FormLabel>
+              <Input
+                type="date"
+                ref={initialRef}
+                placeholder="Дата"
+                name="date"
+                onChange={(event) => {
+                  changeHandler(event);
+                }}
+              />
+            </FormControl>
+
+            {pathname !== '/moneybox' && (
+              <FormControl mt={4}>
+                <FormLabel>Загрузить чек</FormLabel>
+                {/* <Input
 								placeholder="Загрузить чек"
 								name="billUrl"
 								onChange={(event) => {
 									changeHandler(event);
 								}}
 							/> */}
-							<FileUpload
-								isLoading={isLoading}
-								name="billUrl"
-								uploadHandler={uploadHandler}
-							/>
-						</FormControl>
+                <FileUpload isLoading={isLoading} name="billUrl" uploadHandler={uploadHandler} />
+              </FormControl>
+            )}
+            <FormControl mt={4}>
+              <FormLabel>Сумма</FormLabel>
+              <Input
+                placeholder="Сумма"
+                name="paid"
+                onChange={(event) => {
+                  changeHandler(event);
+                }}
+                value={data.paid}
+                // defaultValue={pathname === '/moneybox' ? lastPaid + 0.01 : lastPaid}
+              />
+            </FormControl>
+            {pathname !== '/moneybox' && (
+              <FormControl mt={4}>
+                <FormLabel>Комментарий</FormLabel>
+                <Textarea
+                  name="comment"
+                  placeholder="Комментарий"
+                  size="sm"
+                  onChange={(event) => {
+                    changeHandler(event);
+                  }}
+                />
+              </FormControl>
+            )}
+          </ModalBody>
 
-						<FormControl mt={4}>
-							<FormLabel>Сумма</FormLabel>
-							<Input
-								placeholder="Сумма"
-								name="paid"
-								onChange={(event) => {
-									changeHandler(event);
-								}}
-							/>
-						</FormControl>
-
-						<FormControl mt={4}>
-							<FormLabel>Комментарий</FormLabel>
-							<Textarea
-								name="comment"
-								placeholder="Комментарий"
-								size="sm"
-								onChange={(event) => {
-									changeHandler(event);
-								}}
-							/>
-						</FormControl>
-					</ModalBody>
-
-					<ModalFooter>
-						<Button
-							colorScheme="blue"
-							mr={3}
-							isLoading={isLoading}
-							disabled={isLoading}
-							onClick={() => submitHandler(pathname)}
-						>
-							Добавить
-						</Button>
-						<Button onClick={onClose}>Отменить</Button>
-					</ModalFooter>
-				</ModalContent>
-			</Modal>
-		</>
-	);
+          <ModalFooter>
+            <Button
+              colorScheme="blue"
+              mr={3}
+              isLoading={isLoading}
+              disabled={isLoading}
+              onClick={() => submitHandler(pathname)}>
+              Добавить
+            </Button>
+            <Button onClick={onClose}>Отменить</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 };
 
 export default AddBillModal;
