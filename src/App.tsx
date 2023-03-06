0ed3494c3c64966700a80e9cd8ed383b5509e1f2
denis-snitko <denissnitko@gmail.com>
2023-03-06 16:08:23 +0300

    Remove moneybox page

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,59 @@
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import { theme } from "@chakra-ui/pro-theme";
-import { Route, Routes } from "react-router-dom";
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { theme } from '@chakra-ui/pro-theme';
+import { Route, Routes } from 'react-router-dom';
 
-import "@fontsource/inter/variable.css";
-import Login from "./pages/Login";
-import Home from "./pages/Home";
-import Layout from "./components/Layout";
-import Credit from "./pages/Credit";
-import Repair from "./pages/Repair";
-import { RequireAuth } from "./auth/RequireAuth";
-import NotFound from "./pages/NotFound";
+import '@fontsource/inter/variable.css';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Layout from './components/Layout';
+import Credit from './pages/Credit';
+import Repair from './pages/Repair';
+import { RequireAuth } from './auth/RequireAuth';
+import NotFound from './pages/NotFound';
 import { AuthProvider } from './auth/AuthProvider';
-import Moneybox from "./pages/Moneybox";
 
 export default function App() {
-  const myTheme = extendTheme({
-    colors: { ...theme.colors, brand: theme.colors.purple },
-  }, theme);
+  const myTheme = extendTheme(
+    {
+      colors: { ...theme.colors, brand: theme.colors.purple },
+    },
+    theme
+  );
 
   return (
     <ChakraProvider theme={myTheme}>
       <AuthProvider>
         <Routes>
-          <Route path="/login" element={<Login />} />
+          <Route path='/login' element={<Login />} />
 
-          <Route path="/" element={<Layout />}>
-            <Route index element={<RequireAuth>
-              <Home />
-            </RequireAuth>} />
+          <Route path='/' element={<Layout />}>
             <Route
-              path="credit"
-              element={<RequireAuth>
-                <Credit />
-              </RequireAuth>}
+              index
+              element={
+                <RequireAuth>
+                  <Home />
+                </RequireAuth>
+              }
             />
             <Route
-              path="repair"
-              element={<RequireAuth>
-                <Repair />
-              </RequireAuth>}
+              path='credit'
+              element={
+                <RequireAuth>
+                  <Credit />
+                </RequireAuth>
+              }
             />
-		  <Route
-			  path="moneybox"
-			  element={<RequireAuth>
-				  <Moneybox />
-			  </RequireAuth>}
-		  />
+            <Route
+              path='repair'
+              element={
+                <RequireAuth>
+                  <Repair />
+                </RequireAuth>
+              }
+            />
           </Route>
 
-          <Route path="*" element={<NotFound />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </AuthProvider>
     </ChakraProvider>
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,138 +1,117 @@
-import { ReactNode } from "react";
+import { ReactNode } from 'react';
 import {
-	Box,
-	Flex,
-	HStack,
-	Link,
-	IconButton,
-	Button,
-	useDisclosure,
-	useColorModeValue,
-	Stack,
-	Container,
-} from "@chakra-ui/react";
-import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
-import {
-	Navigate,
-	Outlet,
-	NavLink as RouterLink,
-	useNavigate,
-	useLocation,
-} from "react-router-dom";
-import useAuth from "../auth/useAuth";
-import AddBill from "./AddBill";
+  Box,
+  Flex,
+  HStack,
+  Link,
+  IconButton,
+  Button,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  Container,
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import { Navigate, Outlet, NavLink as RouterLink, useNavigate, useLocation } from 'react-router-dom';
+import useAuth from '../auth/useAuth';
+import AddBill from './AddBill';
 
 const links = [
-	{
-		id: 1,
-		label: "Кредит",
-		path: "/credit",
-	},
-	{
-		id: 2,
-		label: "Ремонт",
-		path: "/repair",
-	},
-	{
-		id: 3,
-		label: "Копилка",
-		path: "/moneybox",
-	},
+  {
+    id: 1,
+    label: 'Кредит',
+    path: '/credit',
+  },
+  {
+    id: 2,
+    label: 'Ремонт',
+    path: '/repair',
+  },
 ];
 
 const NavLink = ({ path, children }: { path: string; children: ReactNode }) => (
-	<Link
-		px={6}
-		py={1}
-		rounded={"sm"}
-		_hover={{
-			textDecoration: "none",
-			bg: useColorModeValue("gray.300", "gray.700"),
-		}}
-		_activeLink={{
-			bg: useColorModeValue("gray.300", "gray.700"),
-		}}
-		as={RouterLink}
-		to={path}
-	>
-		{children}
-	</Link>
+  <Link
+    px={6}
+    py={1}
+    rounded={'sm'}
+    _hover={{
+      textDecoration: 'none',
+      bg: useColorModeValue('gray.300', 'gray.700'),
+    }}
+    _activeLink={{
+      bg: useColorModeValue('gray.300', 'gray.700'),
+    }}
+    as={RouterLink}
+    to={path}>
+    {children}
+  </Link>
 );
 
 export default function Layout() {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const { user, signOut } = useAuth();
-	const { pathname } = useLocation();
-	const { isOpen, onOpen, onClose } = useDisclosure();
+  const { user, signOut } = useAuth();
+  const { pathname } = useLocation();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-	const logoutHandler = () => {
-		signOut(() => navigate("/login"));
-	};
+  const logoutHandler = () => {
+    signOut(() => navigate('/login'));
+  };
 
-	if (!user) {
-		return <Navigate to="/login" />;
-	}
+  if (!user) {
+    return <Navigate to='/login' />;
+  }
 
-	return (
-		<>
-			<Box bg="gray.100" px={4}>
-				<Container maxW="container.2xl">
-					<Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-						<IconButton
-							size={"md"}
-							icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-							aria-label={"Open Menu"}
-							display={{ md: "none" }}
-							onClick={isOpen ? onClose : onOpen}
-						/>
-						<HStack spacing={8} alignItems={"center"}>
-							<HStack
-								as={"nav"}
-								spacing={1}
-								display={{ base: "none", md: "flex" }}
-							>
-								{links.map(({ id, label, path }) => (
-									<NavLink key={id} path={path}>
-										{label}
-									</NavLink>
-								))}
-							</HStack>
-						</HStack>
-						<Flex alignItems={"center"}>
-							<Box me={4}>
-								<AddBill pathname={pathname} />
-							</Box>
-							<Button
-								variant="solid"
-								colorScheme="orange"
-								size="sm"
-								onClick={logoutHandler}
-							>
-								Выйти
-							</Button>
-						</Flex>
-					</Flex>
+  return (
+    <>
+      <Box bg='gray.100' px={4}>
+        <Container maxW='container.2xl'>
+          <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+            <IconButton
+              size={'md'}
+              icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+              aria-label={'Open Menu'}
+              display={{ md: 'none' }}
+              onClick={isOpen ? onClose : onOpen}
+            />
+            <HStack spacing={8} alignItems={'center'}>
+              <HStack as={'nav'} spacing={1} display={{ base: 'none', md: 'flex' }}>
+                {links.map(({ id, label, path }) => (
+                  <NavLink key={id} path={path}>
+                    {label}
+                  </NavLink>
+                ))}
+              </HStack>
+            </HStack>
+            <Flex alignItems={'center'}>
+              <Box me={4}>
+                <AddBill pathname={pathname} />
+              </Box>
+              <Button variant='solid' colorScheme='orange' size='sm' onClick={logoutHandler}>
+                Выйти
+              </Button>
+            </Flex>
+          </Flex>
 
-					{isOpen ? (
-						<Box pb={4} display={{ md: "none" }}>
-							<Stack as={"nav"} spacing={4}>
-								{links.map(({ id, label, path }) => (
-									<NavLink key={id} path={path}>
-										{label}
-									</NavLink>
-								))}
-							</Stack>
-						</Box>
-					) : null}
-				</Container>
-			</Box>
+          {isOpen ? (
+            <Box pb={4} display={{ md: 'none' }}>
+              <Stack as={'nav'} spacing={4}>
+                {links.map(({ id, label, path }) => (
+                  <NavLink key={id} path={path}>
+                    {label}
+                  </NavLink>
+                ))}
+              </Stack>
+            </Box>
+          ) : null}
+        </Container>
+      </Box>
 
-			<Box p={4}>
-				<Container maxW="container.2xl">
-					<Outlet />
-				</Container>
-			</Box>
-		</>
-	);
+      <Box p={4}>
+        <Container maxW='container.2xl'>
+          <Outlet />
+        </Container>
+      </Box>
+    </>
+  );
 }
